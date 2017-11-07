cd07a213612da5b379cadffffdbf68a5b92a066d
1-TA <yekta.y1985@gmail.com>
2017-11-07 14:57:06 -0800

    bug fix

--- create_image.sh
+++ create_image.sh
@@ -86,6 +86,6 @@ source ~/.bash_profile
 # Create /usr/bin/realpath which is used by R to find Java installations
 # NOTE: /usr/bin/realpath is missing in CentOS AMIs. See
 # http://superuser.com/questions/771104/usr-bin-realpath-not-found-in-centos-6-5
-# echo '#!/bin/bash' > /usr/bin/realpath
-# echo 'readlink -e "$@"' >> /usr/bin/realpath
-# chmod a+x /usr/bin/realpath
+echo '#!/bin/bash' > /usr/bin/realpath
+echo 'readlink -e "$@"' >> /usr/bin/realpath
+chmod a+x /usr/bin/realpath
